932eb4672dd82ca066ae443c34205f8e8ae3e9aa
Ravi <ravi97garg@github.com>
2019-09-25 14:01:44 +0530

    UI Improvements

--- src/app/components/adminComponent/edit-college/edit-college.component.ts
+++ src/app/components/adminComponent/edit-college/edit-college.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-// import {CollegeService} from '../../../service/college.service';
-import {ActivatedRoute} from '@angular/router';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
 import {College} from '../../../models/college';
 import {CollegeService} from '../../../services/college.service';
+import csc from 'country-state-city';
+import alert from 'sweetalert2';
+import {AppUrl} from '../../../constants/AppUrl';
 
 @Component({
   selector: 'app-edit-college',
@@ -14,23 +16,27 @@ export class EditCollegeComponent implements OnInit {
 
   collegeFormGroup: FormGroup;
   collegeId: string;
-  initialCollegeData: College;
   loading = false;
+  stateList: any[] = [];
+  cityList: any[] = [];
+  city: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private collegeService: CollegeService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
+    this.stateList = csc.getStatesOfCountry('101');
     this.collegeId = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
-  ngOnInit() {
-    console.log(this.collegeId);
-    this.collegeService.findCollegeById(this.collegeId).subscribe(data => {
-      this.initialCollegeData = data;
-    });
+  onStateChange(value) {
+    this.cityList = csc.getCitiesOfState('' + value);
+    this.collegeFormGroup.get('city').setValue('');
+  }
 
+  ngOnInit() {
     this.collegeFormGroup = this.formBuilder.group({
       collegeName: ['', Validators.required],
       collegeAbbreviation: ['', Validators.required],
@@ -38,18 +44,92 @@ export class EditCollegeComponent implements OnInit {
       address: ['', Validators.required],
       city: ['', Validators.required],
       state: ['', Validators.required],
-      comments: ['', Validators.required],
+      comments: [''],
       faculty: ['', Validators.required],
-      referencePersonName: ['', Validators.required],
-      referencePersonContact: ['', Validators.required]
+      referenceList: this.formBuilder.array([], Validators.required)
     });
+
+    this.collegeService.findCollegeById(this.collegeId).subscribe((data: College) => {
+      const stateId = this.stateList[this.stateList.findIndex((state) => state.id === data.state)].id;
+      this.cityList = csc.getCitiesOfState('' + stateId);
+      console.log(this.cityList);
+      const cityId = this.cityList[this.cityList.findIndex((city) => city.name === data.city)].name;
+      this.city = data.city;
+      this.collegeFormGroup.get('collegeName').setValue(data.collegeName);
+      this.collegeFormGroup.get('collegeAbbreviation').setValue(data.collegeAbbreviation);
+      this.collegeFormGroup.get('universityName').setValue(data.universityName);
+      this.collegeFormGroup.get('address').setValue(data.address);
+      this.collegeFormGroup.get('comments').setValue(data.comments);
+      this.collegeFormGroup.get('faculty').setValue(data.faculty);
+      this.collegeFormGroup.get('state').setValue('' + stateId);
+      this.collegeFormGroup.get('city').setValue(data.city);
+      this.populatedReferDetails(data);
+      this.collegeFormGroup.get('referenceList').setValue(data.referenceList);
+    });
+
+
+  }
+
+  populatedReferDetails(data: College) {
+    data.referenceList.forEach(() => {
+      const categorySelectionArray = this.collegeFormGroup.get('referenceList') as FormArray;
+      categorySelectionArray.push(this.createItem());
+    });
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      designation: ['', Validators.required],
+      contact: ['', Validators.required]
+    });
+  }
+
+
+  get formArr() {
+    return this.collegeFormGroup.get('referenceList') as FormArray;
+  }
+
+  addItem(): void {
+    this.formArr.push(this.createItem());
+  }
+
+  deleteRow(index: number) {
+    this.formArr.removeAt(index);
   }
 
   submitForm() {
-    this.loading = true;
-    this.collegeService.updateCollegeInfo(this.initialCollegeData.id, this.collegeFormGroup.value).subscribe(() => {
-      this.loading = false;
-    });
+    if (this.collegeFormGroup.valid) {
+      this.loading = true;
+      this.collegeService.updateCollegeInfo(this.collegeId, this.collegeFormGroup.value).subscribe((data) => {
+          if (data['errorMessage'] !== null) {
+            this.showToaster(data['errorMessage'], 'error');
+          } else {
+            this.showToaster(data['successMessage'], 'success');
+            setTimeout(() => {
+              this.loading = false;
+              this.router.navigate([AppUrl.VIEW_COLLEGE_ADMIN]);
+            }, 2000);
+          }
+        }, err => {
+          if (err.status === 400) {
+            this.showToaster('Validation failed', 'error');
+
+          } else {
+            this.showToaster(err['error'].message ? err['error'].message : err['error'].text, 'error');
+
+          }
+          this.loading = false;
+        }
+      );
+    }
   }
 
+  showToaster = (message, type) => {
+    alert.fire({
+      title: message,
+      type: type,
+      timer: 1500
+    });
+  }
 }
